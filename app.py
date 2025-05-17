035d202b2cb73f6fac64f876e6312ad8ceb80dda
Doctor-Zer0 <109389618+Doctor-Zer0@users.noreply.github.com>
2025-05-17 15:30:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 # ========== Environment Inputs ==========
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True, origins=['http://127.0.0.1:5500', 'https://galaxypengin.com'])
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'default-fallback-key')
 
 # Spotify app credentials
