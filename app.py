45bc916a569bf157608a8d574ab7d03c94dd19e3
Doctor-Zer0 <109389618+Doctor-Zer0@users.noreply.github.com>
2025-05-17 10:10:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def now_playing():
 
 # Open host and port
 if __name__ == '__main__':
-    port = int(os.environment.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
