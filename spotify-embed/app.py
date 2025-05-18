48542a8b404170de41e645f31b3079d10824c3f4
Doctor-Zer0 <109389618+Doctor-Zer0@users.noreply.github.com>
2025-05-18 11:21:16 -0400

    Update app.py

--- spotify-embed/app.py
+++ spotify-embed/app.py
@@ -43,7 +43,7 @@ def callback():
         }
     )
     token_json = token_res.json()
-    print(token_json)
+    print('Spotify token respose:', token_json)  # DEBUG OUTPUT
     return redirect(f"http://127.0.0.1:5500/mason's%20FUCKING%20WEBSAITE/spotify%20embed/index.html?token={token_json['refresh_token']}")
 
 # Step 3: get current track
@@ -69,7 +69,7 @@ def now_playing():
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     )
-    print(token_res.json())
+    print('token_res:', token_res.json())  # DEBUG OUTPUT
     access_token = token_res.json().get('access_token')
 
     # Fetch now playing
